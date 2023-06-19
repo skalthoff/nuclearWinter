0bd1df667c2e18fcbb37b40a619d00ec0056aeb2
skalthoff <32023561+skalthoff@users.noreply.github.com>
2023-06-18 21:02:41 -0700

    cleaned and finsihed

--- frontend/script.js
+++ frontend/script.js
@@ -1,87 +1,64 @@
-// script.js
+window.onload = () => {
+    const inputGroups = ["usefulCharacters", "uselessCharacters", "usefulAttributes", "uselessAttributes"];
 
-// When the form is submitted
-document.getElementById('characterForm').addEventListener('submit', async (event) => {
-    event.preventDefault();
-
-    // Extract data from form
-    const usefulCharacters = [];
-    const uselessCharacters = [];
-    const usefulAttributes = [];
-    const uselessAttributes = [];
-    for (let i = 0; i < 5; i++) {
-        usefulCharacters.push(document.getElementById('usefulCharacter' + i).value);
-        uselessCharacters.push(document.getElementById('uselessCharacter' + i).value);
-        usefulAttributes.push(document.getElementById('usefulAttribute' + i).value);
-        uselessAttributes.push(document.getElementById('uselessAttribute' + i).value);
-    }
-
-    // Send data to server
-    await fetch('http://localhost:3000/submit', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            usefulCharacters,
-            uselessCharacters,
-            usefulAttributes,
-            uselessAttributes,
-        }),
-    });
-
-    // Clear form for next character sheet
-    for (let i = 0; i < 5; i++) {
-        document.getElementById('usefulCharacter' + i).value = '';
-        document.getElementById('uselessCharacter' + i).value = '';
-        document.getElementById('usefulAttribute' + i).value = '';
-        document.getElementById('uselessAttribute' + i).value = '';
-    }
-});
-
-//When the "Submit All" button is clicked
-document.getElementById('submitAll').addEventListener('click', async (event) => {
-    event.preventDefault();
-
-    // Send "submit all" request to server
-    const response = await fetch('http://localhost:3000/submitAll', {
-        method: 'POST',
-    });
-
-    // Update characterDisplay and characterSelection with response
-    const pairs = await response.json();
-    const characterDisplay = document.getElementById('characterDisplay');
-    const characterSelection = document.getElementById('characterSelection');
-    characterDisplay.innerHTML = '';
-    characterSelection.innerHTML = '';
-    pairs.forEach(pair => {
-        characterDisplay.innerHTML += `<p>${pair.character}: ${pair.attribute}</p>`;
-        const checkbox = document.createElement('input');
-        checkbox.type = 'checkbox';
-        checkbox.id = pair.character;
-        checkbox.name = pair.character;
-        const label = document.createElement('label');
-        label.htmlFor = pair.character;
-        label.appendChild(document.createTextNode(pair.character));
-        characterSelection.appendChild(checkbox);
-        characterSelection.appendChild(label);
-        characterSelection.appendChild(document.createElement('br'));
-    });
-});
-
-// When the "Submit Selection" button is clicked
-document.getElementById('submitSelection').addEventListener('click', (event) => {
-    event.preventDefault();
-
-    // Extract selected characters
-    const checkboxes = document.getElementById('characterSelection').getElementsByTagName('input');
-    const selectedCharacters = [];
-    for (let i = 0; i < checkboxes.length; i++) {
-        if (checkboxes[i].checked) {
-            selectedCharacters.push(checkboxes[i].id);
+    inputGroups.forEach((group) => {
+        for (let i = 0; i < 5; i++) {
+            const inputField = document.createElement("input");
+            inputField.type = "text";
+            inputField.id = `${group}${i}`;
+            document.getElementById(group).appendChild(inputField);
         }
-    }
+    });
 
-    // Log selected characters
-    console.log(selectedCharacters);
-});
+    document.getElementById('characterForm').addEventListener('submit', async (event) => {
+        event.preventDefault();
+
+        const usefulCharacters = [];
+        const uselessCharacters = [];
+        const usefulAttributes = [];
+        const uselessAttributes = [];
+
+        for (let i = 0; i < 5; i++) {
+            usefulCharacters.push(document.getElementById('usefulCharacter' + i).value);
+            uselessCharacters.push(document.getElementById('uselessCharacter' + i).value);
+            usefulAttributes.push(document.getElementById('usefulAttribute' + i).value);
+            uselessAttributes.push(document.getElementById('uselessAttribute' + i).value);
+        }
+
+        await fetch('http://localhost:3000/submit', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                usefulCharacters,
+                uselessCharacters,
+                usefulAttributes,
+                uselessAttributes,
+            }),
+        });
+
+        for (let i = 0; i < 5; i++) {
+            document.getElementById('usefulCharacter' + i).value = '';
+            document.getElementById('uselessCharacter' + i).value = '';
+            document.getElementById('usefulAttribute' + i).value = '';
+            document.getElementById('uselessAttribute' + i).value = '';
+        }
+    });
+
+    document.getElementById('submitAll').addEventListener('click', async (event) => {
+        event.preventDefault();
+
+        const response = await fetch('http://localhost:3000/submitAll', {
+            method: 'POST',
+        });
+
+        const pairs = await response.json();
+        const characterDisplay = document.getElementById('characterDisplay');
+        characterDisplay.innerHTML = '';
+
+        pairs.forEach(pair => {
+            characterDisplay.innerHTML += `<p>${pair.character}: ${pair.attribute}</p>`;
+        });
+    });
+};
